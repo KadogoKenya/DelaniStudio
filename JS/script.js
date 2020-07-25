ab4e80c16f9dfe0e8684a7963d8197968dba27a0
Cathrine Kanini <catherinekanini001@gmail.com>
2020-07-25 16:17:52 +0300

    image two

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,18 @@ $(".imageDetails .img1").mouseout(function() {
     $(".details").hide();
     $(".imageDetails .img1").show();
   });
+// image two
+$(".imageDetails .img2").click(function(){
+    $(".details").show();
+    $(".imageDetails .img2").hide();
+});
+$(".imageDetails .img2").mouseout(function() {
+    $(".details").hide();
+    $(".imageDetails .img2").show();
+  });
+
+//   image three script
+$(".imageDetails .img3").click(function(){
+    $(".details").show();
+    $(".imageDetails .img3").hide();
+});
