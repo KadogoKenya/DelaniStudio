4cea20faf8cfa72f6b29f11a4df0640d28a5ba04
Cathrine Kanini <catherinekanini001@gmail.com>
2020-07-26 00:51:45 +0300

    iamges updates

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,35 @@
 // image one
+$(document).ready(function(){
+    $("#img1").click(function(){
+        $(".details").hide().toggle();
+        $("#img1").slideup();
+    });
+    
+    $(".details").click(function(){
+        $("#img1").slidedown();
+        $(".details").hide();
+    });
 
-$("#img1").click(function(){
-    $("#designDetails").show();
-    $("#img1").hide();
+    // image two
+    $(".img2").click(function(){
+        $(".details").show();
+        $(".img2").hide();
+    });
+    $(".details").click(function(){
+        $(".img2").show();
+        $(".details").hide();
+    });
+
+
+    //   image three script
+    $(".img3").click(function(){
+        $(".dddetails").show();
+        $(".img3").hide();
+    });
+    $(".dddetails").click(function() {
+        $(".dddetails").hide();
+        $(".img3").show();
+    });
+
+
 });
-$("#designDetails").click(function(){
-    $("#img1").show();
-    $("#designDetails").hide();
-});
-
-// image two
-$(".img2").click(function(){
-    $(".details").show();
-    $(".img2").hide();
-});
-$(".details").click(function(){
-    $(".img2").show();
-    $(".details").hide();
-});
-
-
-//   image three script
-$(".img3").click(function(){
-    $(".dddetails").show();
-    $(".img3").hide();
-});
-$(".dddetails").click(function() {
-    $(".dddetails").hide();
-    $(".img3").show();
-  });
-
-
