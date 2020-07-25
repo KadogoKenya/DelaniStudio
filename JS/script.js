dc8eb101f31be3cec36ed0d2d8902314bb059919
Cathrine Kanini <catherinekanini001@gmail.com>
2020-07-25 16:11:40 +0300

    script changes

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 
-$(".imgDetails .img1").click(function(){
+$(".imageDetails .img1").click(function(){
     $(".details").show();
-    $(".imgDetails .img1").hide();
+    $(".imageDetails .img1").hide();
 });
-$(".imgDetails .img1").mouseout(function() {
+$(".imageDetails .img1").mouseout(function() {
     $(".details").hide();
-    $(".imgDetails .img1").show();
+    $(".imageDetails .img1").show();
   });
