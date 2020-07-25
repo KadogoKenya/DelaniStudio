d97d2e940a2d987526f5b5e0d19c8eb456eff4b3
Cathrine Kanini <catherinekanini001@gmail.com>
2020-07-25 08:48:04 +0300

    script

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,7 @@ $(".imgDetails .img1").click(function(){
     $(".details").show();
     $(".imgDetails .img1").hide();
 });
+$(".imgDetails .img1").mouseout(function() {
+    $(".details").hide();
+    $(".imgDetails .img1").show();
+  });
